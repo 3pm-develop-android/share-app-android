1156c6fd76c2b1b6a2a7fbb9c2d174ed11a8b170
tuna2 <tuna2209@gmail.com>
2023-03-16 15:50:37 +0700

    add SaData.kt

---
+++ sharingapp/src/main/java/tpm/yu/mol/share/bin/SaData.kt
@@ -0,0 +1,35 @@
+package tpm.yu.mol.share.bin
+
+class SaData(val viewType: BaseData) {
+    /* ************************************************************
+     * Classes
+     ************************************************************ */
+    enum class Direction {
+        VERTICAL, HORIZONTAL
+    }
+
+    enum class Quantity {
+        ONE, MANY
+    }
+
+    sealed class BaseData(
+        open var quantity: Quantity
+    )
+
+    /* ************************************************************
+     * List
+     ************************************************************ */
+    sealed class ListData(open var direction: Direction) : BaseData(quantity = Quantity.MANY)
+
+    class List(override var direction: Direction) : ListData(direction = direction)
+
+    class Grid(override var direction: Direction, val count: Int = 3) :
+        ListData(direction = direction)
+
+    class Pager(override var direction: Direction) : ListData(direction = direction)
+
+    /* ************************************************************
+     * One
+     ************************************************************ */
+    sealed class OneType(open var direction: Direction) : BaseData(quantity = Quantity.ONE)
+}
